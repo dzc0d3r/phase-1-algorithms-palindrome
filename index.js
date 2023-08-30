9726b81837cd23e71affc520ef5a2324b7c07745
dzc0d3r <m4dcod3r@protonmail.com>
2023-08-30 23:48:21 +0100

    Completed assignements :tada:

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // make sure the string is all in lower case
+  word.toLowerCase()
+  return word === word.split("").reverse().join("")
 }
 
 /* 
   Add your pseudocode here
+  make sure the string is all in lower case
+  compare the string (ltr) and its reversed string (rtl)
+  if it's the same return true 
+  otherwise false
 */
 
 /*
   Add written explanation of your solution here
+  if the reverse string is equal to the normal string 
+  the word is a palindrome
+  otherwise it's not 
 */
 
 // You can run `node index.js` to view these console logs
